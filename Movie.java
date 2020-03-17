11ec60b6f57ea6ddb941627d056ffacd9a76ff4f
meaganruan <54692293+meaganruan@users.noreply.github.com>
2020-03-17 16:23:12 -0400

    Add files via upload

--- Movie.java
+++ Movie.java
@@ -1,40 +1,52 @@
 package cisc3130a3;
+
 public class Movie {
-  private String title;
-  private int releaseYear;
-  Movie leftchild;
-  Movie rightchild;
-  //constructor for the Movie class
-  public Movie(String t, int ry){
-    title=t;
-    releaseYear=ry;
-  
-}
-  //getter and setter methods for the private fields.
-    public String getTitle(){
+
+    String title; //key
+    int releaseYear;
+    Movie leftChild;
+    Movie rightChild;
+    //constructor for the Movie class
+
+    public Movie(String t, int ry) {
+        title = t;
+        releaseYear = ry;
+
+    }
+    //getter and setter methods for the private fields.
+
+    public String getTitle() {
         //only takes the title from the beginning of the String to the empty space before the parenthesis of the year
-        return title.substring(0, ')'-1);
+        return title.substring(0, '1' - 2);
     }
-    public Integer getReleaseYear(){
+
+    public Integer getReleaseYear() {
         //parsed the year of the title into int to work with integers.
-        return releaseYear=Integer.parseInt(title.substring('('+1,')'));
+        return releaseYear = Integer.parseInt(title.substring('1', ')'));
     }
-    public Movie getLeftChild(){
-        return leftchild;
+
+    public Movie getLeftChild() {
+        return leftChild;
     }
-    public Movie getRightChild(){
-        return rightchild;
+
+    public Movie getRightChild() {
+        return rightChild;
     }
-    public void setTitle(String d){
-        title=d;
+
+    public void setTitle(String d) {
+        title = d;
     }
-    public void setReleaseYear(Integer ry){
-        releaseYear=ry;
+
+    public void setReleaseYear(Integer ry) {
+        releaseYear = ry;
     }
-    public void setLeftChild (Movie lc){
-        leftchild=lc;
+
+    public void setLeftChild(Movie lc) {
+        leftChild = lc;
     }
-    public void setRightChild(Movie rc){
-        rightchild=rc;
+
+    public void setRightChild(Movie rc) {
+        rightChild = rc;
+
     }
 }
--- MovieBST.java
+++ MovieBST.java
@@ -1,33 +1,69 @@
 package cisc3130a3;
 
-import java.util.*;
-
 public class MovieBST {
-  private Movie first;
 
-  public void subSet(String start, String end){
-    // Selects movie titles that fall alphabetically between start and end.
-    
-  }
+    private Movie root;
 
-  // more methods
-  public void addMovieNode(String t, int ry){
-      Movie newNode = new Movie(t, ry);
-      
-      if (first == null){
-          first = newNode;
-      }
-      else{
-          Movie root = first;
-          
-          Movie parent;
-          while(true){
-              parent = root;
-              //divide nodes into left and right child
-                  
-              
-          }
-      }
-  }
+    public void subSet(String start, String end) {
+        // Selects movie titles that fall alphabetically between start and end.
+        if (start.charAt(0) < end.charAt(0)) {
+            //do something
+        } else if (start.charAt(0) > end.charAt(0)) {
+            //do something
+        } else {//if the character at position 0 are equal for both Strings.
+            if (start.length() >= end.length()) {
+                for (int i = 1; i < start.length(); i++) {
+                    if (start.charAt(i) != end.charAt(i)) {
+                        //do something
+                    }
+                }
+            } else if (start.length() <= end.length()) {
+                for (int j = 1; j < end.length(); j++) {
+                    if (start.charAt(j) != end.charAt(j)) {
+                        //do something
+                    }
+                }
+            }
+
+        }
+    }
+
+    //takes in the Movie node
+    public void addMovieNode(String t, int ry) {
+        Movie newNode = new Movie(t, ry);
+
+        //checks if there is already a parent node or  not
+        if (root == null) {
+            root = newNode;
+        } else {
+            //if there already is a node, it will be the first (root node)
+            Movie first = root;
+
+            //create a nother movie object
+            Movie parent;
+            //infinite loop
+            while (true) {
+                parent = first;
+                //divide nodes into left and right child
+                //left child if t is less than the value of the first node's title's first char  
+                if (t.charAt(0) < ((first.title).charAt(0))) {
+                    first = first.leftChild;
+                    //if there is no root
+                    if (first == null) {
+                        parent.leftChild = newNode;
+                        return;
+                    }
+                } else {
+                    first = first.rightChild;
+
+                    if (first == null) {
+                        parent.rightChild = newNode;
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
 
 }
