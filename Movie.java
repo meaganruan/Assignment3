474bbbcdffd67d23c2b6d68130b78c894db4096d
meaganruan <54692293+meaganruan@users.noreply.github.com>
2020-03-19 18:17:56 -0400

    Update Movie.java

--- Movie.java
+++ Movie.java
@@ -25,27 +25,27 @@ public class Movie {
         //parsed the year of the title into int to work with integers.
         return releaseYear = Integer.parseInt(title.substring('1', ')'));
     }
-
+        //getting value of left child
     public Movie getLeftChild() {
         return leftChild;
     }
-
+        //getting value of the right child
     public Movie getRightChild() {
         return rightChild;
     }
-
+        //setting title
     public void setTitle(String d) {
         title = d;
     }
-
+        //setting the release year
     public void setReleaseYear(Integer ry) {
         releaseYear = ry;
     }
-
+        //setting the left child
     public void setLeftChild(Movie lc) {
         leftChild = lc;
     }
-
+        //setting the right child
     public void setRightChild(Movie rc) {
         rightChild = rc;
 
